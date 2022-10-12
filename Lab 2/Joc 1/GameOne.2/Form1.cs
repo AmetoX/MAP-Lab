31f5967f711ebe545f98939effb7dec5b93dc74a
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-10-12 19:55:42 +0300

    x si zero

---
+++ Lab 2/Joc 1/GameOne.2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOne._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button[,] buttons;
+        int n = 3;
+        int player = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttons = new Button[n, n];
+            int size = pictureBox1.Width / 3;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Button button = new Button();
+                    button.Parent = pictureBox1;
+                    button.Size = new Size(size, size);                   
+                    button.Location = new Point(j * size, i * size);
+                    button.Font = new Font("Arial", 30);
+                    button.Click += gridButton_Click;
+
+                    buttons[i, j] = button;
+                }
+            }
+        }
+        private void gridButton_Click(object sender , EventArgs e)
+        {
+            Button button = sender as Button;
+            if (player == 1)
+            {
+                button.Text = "X";
+                player = 2;
+                button.Enabled = false;
+            }
+            else
+            {
+                button.Text = "O";
+                player = 1;
+                button.Enabled = false;
+            }
+            if (CheckGameWin())
+            {
+                MessageBox.Show("Win");
+            }
+            else if (CheckGameLost())
+            {
+                MessageBox.Show("Remiza");
+            }
+        }
+        bool CheckGameWin()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int sumaX = 0, sumaY = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (buttons[i, j].Text == "X")
+                        sumaX++;
+                    if (buttons[i,j].Text == "Y")
+                        sumaY++;
+                }
+                if(sumaX == 3 || sumaY == 3)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        bool CheckGameLost()
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (buttons[i, j].Enabled)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Lab 2/Joc 1/GameOne/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        Graphics graphics;
+        Bitmap bitmap;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.Red);
+            Brush brush = new SolidBrush(Color.Black);
+            graphics.DrawString("yo",new Font("Arial",20),brush,NewPoint(0,0));
+            pictureBox1.Image = bitmap;
+        }
+        Point NewPoint(int x , int y)
+        {
+            return new Point(pictureBox1.Width / 2 + x, pictureBox1.Height / 2 - y);
+        }
+    }
+}
