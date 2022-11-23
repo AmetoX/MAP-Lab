ab3e12f5bae6764466646614034eaca40c7c016c
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-11-23 20:32:02 +0200

    platformer

---
+++ Lab 2/Platformer/Engine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platformer
+{
+    public class Engine
+    {
+        public static Form1 form;
+        public static void Initialize(Form1 f)
+        {
+            form = f;
+        }      
+
+        public static void PlayerCollision()
+        {
+            form.player.isJumping = true;
+            foreach (var control in form.Controls)
+            {
+                if(control is PictureBox && !control.Equals(form.player.image))
+                {
+                    if(form.player.image.Bounds.IntersectsWith
+                        ((control as PictureBox).Bounds))
+                    {
+                        form.player.gravity = 0;
+                        form.player.isJumping = false;
+                        form.player.image.Top = (control as PictureBox).Top
+                            - form.player.image.Height + 1;
+
+                    }
+
+                }
+            }
+        }
+
+        public static void CheckIfYouLose()
+        {
+            if(form.player.image.Top > form.Height)
+            {
+                form.timer1.Enabled = false;
+               var response = MessageBox.Show("You fell off and died. Game OVER !\n Do you want to restart?","You lose",
+                    MessageBoxButtons.YesNo);
+                if(response == DialogResult.Yes)
+                {
+                    form.player.image.Parent = null;
+                    form.player = new Player();
+                    form.timer1.Enabled = true;
+                }
+                else
+                {
+                    form.Close();
+                }
+                
+            }
+        }
+    }
+}
---
+++ Lab 2/Platformer/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Platformer
+{
+    public class Player
+    {
+        public PictureBox image;
+        public int speed = 5,  gravity;
+        public bool isMovingLeft, isMovingRight, isJumping = true;
+        public const int maxGravity = 25;
+
+        public Player() 
+        {
+            image = new PictureBox();
+            image.Parent = Engine.form;
+            image.Location = new Point(Engine.form.Width /2 , Engine.form.Height * 3 / 4);
+            image.Size = new Size(100, 100);
+            image.BackColor = Color.Transparent;
+            image.Image = Image.FromFile("../../Resources/Player.png");
+            image.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+        public void Move()
+        {
+            if(isMovingLeft)
+            {
+                image.Left -= speed;
+            }
+            else if (isMovingRight)
+            {
+                image.Left += speed;
+            }
+            gravity = Math.Min(maxGravity, gravity + 1);
+            image.Top += gravity;
+            Engine.PlayerCollision();
+        }
+
+    }
+}
